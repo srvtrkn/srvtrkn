f4aa2296f485842f3b5f9cfd3ee43de242d4020e
dtserkan <servet.erkan@digiturk.com.tr>
2018-09-20 13:29:46 +0300

    personal page code

---
+++ src/Components/Footer.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    if (this.props.data) {
+      var networks = this.props.data.social.map(function(network) {
+        return (
+          <li key={network.name}>
+            <a href={network.url} target="_blank">
+              <i className={network.className} />
+            </a>
+          </li>
+        );
+      });
+    }
+
+    return (
+      <footer>
+        <div className="row">
+          <div className="twelve columns">
+            <ul className="social-links">{networks}</ul>
+
+            <ul className="copyright">
+              <li>&copy; Copyright 2018 SRVTRKN</li>
+            </ul>
+          </div>
+          <div id="go-top">
+            <a className="smoothscroll" title="Back to Top" href="#home">
+              <i className="icon-up-open" />
+            </a>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
